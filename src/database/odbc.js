9c3c64917ce028da4f78067ffa2b5f2101ff0f18
Eric <ericfragoso@gmail.com>
2019-04-11 17:49:50 -0300

    mudando conexão

--- src/database/odbc.js
+++ src/database/odbc.js
@@ -1,61 +1,76 @@
-let connection;
-var oracledb = require("oracledb");
+// Using a fixed Oracle time zone helps avoid machine and deployment differences
+process.env.ORA_SDTZ = 'UTC';
 
-(async function(){
-    try{
-        connection = await oracledb.getConnection({
-            user:'leitura',
-            password:'leitura',
-            connectString:'10.0.1.3:1521/MEGA'
-        });
-        console.log("conectou blz");
+var oracledb = require('oracledb');
 
-        result = await connection.execute(
-            `select vp.ANO, 
-                    vp.MES,
-                    to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')) as SEMANA, 
-                    vp.DATA,       
-                    decode(upper(substr(vp.SAFRA,1,1)),
-                        'M','Manga'
-                        ,'U','Uva'
-                        ,'C','Cacau','Outra') as CULTURA,
+async function run() {
+  let connection;
+
+  try {
+
+    let sql, binds, options, result;
+
+    connection = await oracledb.getConnection(  {
+      user          : 'leitura',
+      password      : 'leitura',
+      connectString : '10.0.1.3:1521/MEGA'
+    });
+
+   
+
+    sql = `select vp.ANO,
+            vp.MES,
+            to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')) as SEMANA,
+            vp.DATA,
+            decode(upper(substr(vp.SAFRA,1,1)),'M','Manga'
+                                            ,'U','Uva'
+                                            ,'C','Cacau','Outra') as CULTURA,
             vp.VARIEDADE,
             vp.CONTROLE,
             sum(vp.PESO) as VOLUME_KG
-                                                                                                                         
-            from mgagr.agr_bi_visaoprodutivaph_dq vp
-            where vp.PROCESSO = 1
-            group by
-                vp.ANO,
-                vp.MES,
-                to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')),
-                vp.DATA,
-                decode(upper(substr(vp.SAFRA,1,1)),'M','Manga'
-                                        ,'U','Uva'
-                                        ,'C','Cacau','Outra'),
-                vp.VARIEDADE,
-                vp.CONTROLE`,
-                [],  
-           function(err, result) {
-              if (err) {
-                console.error(err.message);
-                return;
-              }
-              console.log(result.rows);
-           })
-        });
+                                                                                                                            
+        from mgagr.agr_bi_visaoprodutivaph_dq vp
+        where vp.PROCESSO = 1
+        group by
+            vp.ANO,
+            vp.MES,
+            to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')),
+            vp.DATA,
+            decode(upper(substr(vp.SAFRA,1,1)),'M','Manga'
+                                            ,'U','Uva'
+                                            ,'C','Cacau','Outra'),
+            vp.VARIEDADE,
+            vp.CONTROLE
+        `;
 
-    } catch(err){
-        console.log("Error: ", err)
-    }finally{
-        if (connection){
-            try{
-                await connection.close();
-            } catch(err){
-                console.log("erro fechando a conexão:", err);
-            }
-        }
+    binds = {};
+
+    // For a complete list of options see the documentation.
+    options = {
+      outFormat: oracledb.OBJECT   // query result format
+      // extendedMetaData: true,   // get extra metadata
+      // fetchArraySize: 100       // internal buffer allocation size for tuning
+    };
+
+    result = await connection.execute(sql, binds, options);
+
+    console.log("Column metadata: ", result.metaData);
+    console.log("Query results: ");
+    console.log(result.rows);
+
+
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
     }
+  }
+}
 
-})()
+run();
 
