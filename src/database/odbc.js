ef4caa267b5b16b400abd3ee5c76edbb18f95034
Eric <ericfragoso@gmail.com>
2019-04-12 14:23:34 -0300

    query individual uva5

--- src/database/odbc.js
+++ src/database/odbc.js
@@ -30,7 +30,7 @@ async function run() {
             sum(vp.PESO) as VOLUME_KG
                                                                                                                             
         from mgagr.agr_bi_visaoprodutivaph_dq vp
-        where vp.PROCESSO = 1 AND vp.CONTROLE = 8 AND vp.ANO = 2019 AND CULTURA = Uva
+        where vp.PROCESSO = 1 AND vp.CONTROLE = 8 AND vp.ANO = 2019 AND decode(upper(substr(vp.SAFRA,1,1)),'M','Manga','U','Uva','C','Cacau','Outra') = Uva
         group by
             vp.ANO,
             vp.MES,
