594be48507ccab9d99c71e293b08710fa6f45beb
Eric <ericfragoso@gmail.com>
2019-04-12 14:52:39 -0300

    query testando codigo do fornecedor 3

--- src/database/odbc.js
+++ src/database/odbc.js
@@ -32,7 +32,7 @@ async function run() {
             sum(vp.PESO) as VOLUME_KG
                                                                                                                             
         from mgagr.agr_bi_visaoprodutivaph_dq vp
-        where vp.PROCESSO = 1 
+        where vp.PROCESSO = 1 AND vp.COD_FORNECEDOR = 87
         group by
             vp.COD_FORNECEDOR,
             vp.ANO,
