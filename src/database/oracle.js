19e007fc654de23c8cda5b4fb06b67c01ad15efe
Eric <ericfragoso@gmail.com>
2019-04-12 15:51:11 -0300

    modulando oracle

--- src/database/oracle.js
+++ src/database/oracle.js
@@ -51,6 +51,8 @@ async function busca(sql){
             console.log("Column metadata: ", result.metaData);
             console.log("Query results: ");
             console.log(result.rows);
+
+            return (result.rows);
         // final das ações de sucesso
         
     } catch (err) {
@@ -68,33 +70,7 @@ async function busca(sql){
 }
 
 
-busca(`select vp.COD_FORNECEDOR,
-vp.ANO,
-vp.MES,
-to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')) as SEMANA,
-vp.DATA,
-decode(upper(substr(vp.SAFRA,1,1)),'M','Manga'
-                                ,'U','Uva'
-                                ,'C','Cacau','Outra') as CULTURA,
-vp.VARIEDADE,
-vp.CONTROLE,
-vp.SAFRA,
-sum(vp.PESO) as VOLUME_KG
-                                                                                                                    
-from mgagr.agr_bi_visaoprodutivaph_dq vp
-where vp.PROCESSO = 1 AND vp.COD_FORNECEDOR = 83
-group by
-    vp.COD_FORNECEDOR,
-    vp.ANO,
-    vp.MES,
-    to_number(to_char(to_date(vp.DATA,'DD/MM/YYYY'),'WW')),
-    vp.DATA,
-    decode(upper(substr(vp.SAFRA,1,1)),'M','Manga'
-                                    ,'U','Uva'
-                                    ,'C','Cacau','Outra'),
-    vp.VARIEDADE,
-    vp.CONTROLE,
-    vp.SAFRA
-order by vp.DATA
-`);
+busca(`select  f.FAGR_IN_CODIGO AS COD_FORNECEDOR,
+f.FAGR_ST_NOME AS FORNECEDOR
+from mgagr.AGR_FAGRICOLA f`);
 module.exports = oracledb;
