c31729309f73d1b24d1c855d6f89423048ea73b8
Eric <ericfragoso@gmail.com>
2019-04-22 09:48:22 -0300

    Avançando com controles

--- src/app/controllers/controlesController.js
+++ src/app/controllers/controlesController.js
@@ -24,7 +24,7 @@ router.post('/', async(req, res)=>{
     try{
         if (await Controle.findOne({codigo}))
             return res.status(400).send({error: 'Controle já importado anteriormente'});
-        const controle = await Controle.create({...req.body, user:req.userId});
+        const controle = await Controle.create({...req.body, importadoPor:req.userId});
         
         return res.send({
             controle
