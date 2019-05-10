ff02e744f9744fddb5c2f3eab998e0acc5e63770
Eric <ericfragoso@gmail.com>
2019-05-10 11:56:31 -0300

    Ajustando Galeria de imagens qualidade

--- src/app/controllers/controlesController.js
+++ src/app/controllers/controlesController.js
@@ -82,7 +82,7 @@ router.post('/gallery', upload.array('file'), async(req, res)=>{
     let gallery = [];
     let itens = [];
     req.files.map((image)=>itens.push({'url':`http://138.204.68.18:3323/enviadas/${image.filename}`}));
-    gallery.push(itens);
+    gallery = req.files;
     res.status(200).json(gallery);
 });
 
