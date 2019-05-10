f5b48334e1cb9c170b2c8a6d6578b6a20dcfb187
Eric <ericfragoso@gmail.com>
2019-05-10 17:45:56 -0300

    Ajustando Galeria de imagens qualidade

---
+++ src/app/controllers/imageController.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth')
+const router = express.Router();
+const Image = require('../models/images');
+
+router.use(authMiddleware);
+
+router.get('/', async (req, res)=>{
+    try{
+        const images = await Image.find();
+
+        return res.send({Images});
+    }catch(err){
+        return res.status(400).send({error:'Erro carregando usuários'});
+    }
+});
+
+
+module.exports = app => app.use('/images', router);
