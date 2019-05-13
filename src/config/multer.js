ddd3b0a6ff1c5eb6b430fa202e2edf54255b2a42
Eric <ericfragoso@gmail.com>
2019-05-13 18:40:41 -0300

    Ajustando Galeria de imagens qualidade

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,25 +3,6 @@ const path = require('path');
 const crypto = require('crypto');
 
 var destino=path.resolve(__dirname,'..', 'uploads');
-/*var etapa=localStorage.getItem("etapa"); 
-var controle = localStorage.getItem("controle"); 
-var fornecedorAtual = localStorage.getItem("fornecedorAtual"); 
-
-switch etapa {
-    case "RECEPÇÃO":
-        destino = path.resolve(__dirname,'..', 'uploads', 'recepcao');
-    break;
-    case "SELEÇÃO":
-        destino = path.resolve(__dirname,'..', 'uploads', 'selecao');
-    break;
-    case "EMBALAMENTO":
-        destino = path.resolve(__dirname,'..', 'uploads', 'embalamento');
-    break;
-    case "EXPEDIÇÃO":
-        destino = path.resolve(__dirname,'..', 'uploads', 'expedicao');
-    break;
-}*/
-
 
 module.exports = {
     dest: destino,
@@ -33,7 +14,6 @@ module.exports = {
             crypto.randomBytes(6, (err, hash)=>{
                 if(err) cb(err);
 
-                //const fileName = `${fornecedorAtual}-${controle}-${hash.toString('hex')}-${file.originalname}`;
                 const fileName = `${hash.toString('hex')}-${file.originalname}`;
 
                 cb(null, fileName);
