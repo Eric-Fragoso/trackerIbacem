b5d270505260e362b64605cfce22016b26308e63
Eric <ericfragoso@gmail.com>
2019-06-23 16:14:20 -0300

    Finalizando controles produtor

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -22,7 +22,7 @@ const UserSchema = new mongoose.Schema({
         select:false,
     },
     telefone:{
-        tyep:String,
+        type:String,
         required:false,
     },
     fidelidade:{
